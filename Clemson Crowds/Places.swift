1a89e63e547cc7da6eda3937039588a4530cef48
Shannon Beck <Shannon.e.beck@gmail.com>
2017-01-29 16:16:36 -0500

    Added latitudes and longitudes for certain places for future use

---
+++ Clemson Crowds/Places.swift
@@ -0,0 +1,112 @@
+//
+//  Places.swift
+//  Clemson Crowds
+//
+//  Created by Shannon Beck on 1/29/17.
+//  Copyright © 2017 Shannon Beck. All rights reserved.
+//
+
+import Foundation
+
+// Each location has:
+/*  Name
+ *  Latitude
+ *  Longitude
+ *  Current crowd level
+ *  last updated time
+ *  last 3 updates: (time, crowd level)
+ /*
+
+
+/*
+ Clemson Cooper Library
+ Latitude: 34.676673
+ Longitude: -82.836364
+ 
+
+ Starbucks (Cooper Library)
+ Latitiude: 34.676673
+ Longitude: -82.836364
+
+ 
+ Starbucks (Core)
+ Latitiude: 34.679546
+ Longitude: -82.839420
+ 
+
+ Hendrix Food Court
+ Latitiude: 34.676180
+ Longitude: -82.832150
+ 
+ 
+ Schilletter Dining Hall
+ Latitiude: 34.676908
+ Longitude: -82.832865
+ 
+ 
+ Wendy's
+ Latitude: 34.677134
+ Longitude: -82.832857
+ 
+ 
+ Core Dining Hall
+ Latitiude: 34.679361
+ Longitude: -82.839730
+ 
+ 
+ Einstein Bros. Bagels
+ Latitiude: 34.676291
+ Longitude: -82.831941
+ 
+ 
+ Bus Stop at Sikes (away from Downtown)
+ Latitiude: 34.679501
+ Longitude: -82.834776
+ 
+
+ Bus Stop at Sikes (towards Downtown)
+ Latitiude: 34.679688
+ Longitude: -82.834676
+ 
+
+ Bus Stop at Cherry Road (towards Downtown)
+ Latitiude: 34.677594
+ Longitude: -82.830099
+ 
+ 
+ Bus Stop at Cherry Road (away from Downtown)
+ Latitiude: 34.677824
+ Longitude: -82.830347
+
+ 
+ McAdams Hall Computer Lab
+ Latitiude: 34.675962
+ Longitude: -82.834376
+ 
+
+ Academic Success Center Study Areas
+ Latitiude: 34.675845
+ Longitude: -82.835782
+ 
+ 
+ Union Food Court
+ Latitiude: 34.679773
+ Longitude: - 82.838469
+ 
+ 
+ Fike Recreation Center
+ Latitiude: 34.680638
+ Longitude: -82.841980
+
+ */*/*/
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
